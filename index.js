06a3973763872484b4fe48744586db807de0c59b
Mind Roots <mgupta@mind-roots.com>
2021-10-28 12:23:17 +0530

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
+const post = process.env.PORT || 3000
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(3000);
+}).listen(post);
